187ed8a78cbbb5dd368d536a84c578cf931886f0
DanielFGray <DanielFGray@gmail.com>
2017-05-24 18:59:17 -0500

    lift initial payload into root component

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,33 +1,21 @@
 // @flow
-import React, { Component } from 'react'
+import React from 'react'
 import { injectState } from 'freactal'
 
 import style from './Home.sss'
 import PostExcerpt from '../components/Post'
 
-const Loader = () => (
-  <div children="Loading" />
-)
-
-class Home extends Component {
-  componentDidMount() {
-    if (this.props.state.excerpts.length < 1) {
-      this.props.effects.getPosts()
-    }
+const Home = ({ state }) => {
+  if (state.postsPending) {
+    return null
   }
 
-  render() {
-    if (this.props.state.postsPending) {
-      return <Loader />
-    }
-
-    return (
-      <div className={style.excerpts}>
-        {this.props.state.excerpts.map(e =>
-          <PostExcerpt key={`${e.title}${e.date}`} {...e} />)}
-      </div>
-    )
-  }
+  return (
+    <div className={style.excerpts}>
+      {state.excerpts.map(e =>
+        <PostExcerpt key={`${e.title}${e.date}`} {...e} />)}
+    </div>
+  )
 }
 
 export default injectState(Home)
--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import {
   BrowserRouter as Router,
   Route,
 } from 'react-router-dom'
+import { injectState } from 'freactal'
 
 import './style.sss'
 
@@ -16,18 +17,34 @@ import Tags from './containers/Tags'
 
 import { Provider } from './actions'
 
-const App = Provider(() => (
-  <Router>
-    <div>
-      <Nav />
-      <Route path="/" exact component={Home} />
-      <Route path="/tags" exact component={Home} />
-      <Route path="/tags/:tag" component={Tags} />
-      <Route path="/:category" exact component={Categories} />
-      <Route path="/:category/:title" component={Post} />
-    </div>
-  </Router>
-))
+const App = Provider(injectState(
+  class extends React.Component {
+    componentDidMount() {
+      this.getPosts()
+    }
+
+    componentWillReceiveProps() {
+      this.getPosts()
+    }
+
+    getPosts = () =>
+      this.props.effects.getPosts()
+
+    render() {
+      return (
+        <Router>
+          <div>
+            <Nav />
+            <Route path="/" exact component={Home} />
+            <Route path="/tags" exact component={Home} />
+            <Route path="/tags/:tag" component={Tags} />
+            <Route path="/:category" exact component={Categories} />
+            <Route path="/:category/:title" component={Post} />
+          </div>
+        </Router>
+      )
+    }
+  }))
 
 document.addEventListener('DOMContentLoaded', () =>
   render(<App />, document.getElementById('root')))
