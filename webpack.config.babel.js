e359b49a6e4179f52be64e696e240a7a038d253e
DanielFGray <DanielFGray@gmail.com>
2017-05-30 00:42:42 -0500

    use proper routes and title

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -37,7 +37,7 @@ const plugins = [
   new HtmlWebpackPlugin({
     template: 'src/index.ejs',
     inject: false,
-    title: 'title',
+    title: 'DanielFGray',
     appMountId: 'root',
     devServer: '',
   }),
